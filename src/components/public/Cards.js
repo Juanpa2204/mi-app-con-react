abe6043cccf56e981d2ad3ab3bc82fee04e765ec
pablo rojas <pablorojas654@gmail.com>
2023-03-27 22:06:50 -0300

    act

--- src/components/public/Cards.js
+++ src/components/public/Cards.js
@@ -31,8 +31,6 @@ export const Cards = () => {
             });
     };
 
-
-
     const handleSearch = (event) => {
         event.preventDefault();
         setSearchTerm(event.target.value.toLowerCase());
